425bb75a27fafcdf02a2bcbbd985f8b29058f382
Sunil Rehman <sunil.rehman@gmail.com>
2019-12-15 14:05:59 -0800

    Made "prioritized" replay optional

--- p1_navigation/params.py
+++ p1_navigation/params.py
@@ -45,7 +45,8 @@ class DQNParameters:
                  alpha=0.6,                              # Exponent for computing priorities in replay buffer
                  error_clipping=False,                   # Flag for limiting the TD error to between -1 and 1
                  gradient_clipping=False,                # Flag for clipping the norm of the gradient to 1
-                 reward_clipping=False,                   # Flag for limiting the reward to between -1 and 1
+                 reward_clipping=False,                  # Flag for limiting the reward to between -1 and 1
+                 prioritized_replay_buffer=True,         # use a prioritized replay buffer
                  epsilon=VarParam(1.0, 0.01, 0.005),     # Epsilon-greedy selection of an action. Probably for selecting a random action from the QTable
                  gamma=VarParam(0.9, 0.99, 0.01),        # Discount factor (0 to 1). Higher values favor long term over current rewards.
 
@@ -53,10 +54,6 @@ class DQNParameters:
                                                          # increase to 1.0 as per Schauel et al. https://arxiv.org/abs/1511.05952
                  tau=VarParam(0.001, 0.1, 0.00001)       # weighting factor for soft updating the target network in DQN.
                  ):
-        self.epsilon = epsilon
-        self.gamma = gamma
-        self.beta = beta
-        self.tau = tau
         self.state_size = state_size
         self.action_size = action_size
         self.seed = seed
@@ -69,6 +66,11 @@ class DQNParameters:
         self.error_clipping = error_clipping
         self.gradient_clipping = gradient_clipping
         self.reward_clipping = reward_clipping
+        self.prioritized_replay_buffer = prioritized_replay_buffer
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.beta = beta
+        self.tau = tau
 
     def __str__(self):
         return "lr={}, epsilon={:.3f}, gamma={:.3f}, beta={:.3f}, tau={:.3f}".format(
